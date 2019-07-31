3263cf91368aa7d1f73b6f8c704151721dec18f0
Brian Bochicchio <bbochicchio@gmail.com>
2019-07-31 11:42:14 -0500

    Added feature, modified console outputs and removed unnecessary code.
    Can now include an output filename arg.  Ex. wegner2file.js 5001 song.txt
    getLogTime now returns an ISOString
    Removed the unnecessary "Hello World"

--- wegner2file.js
+++ wegner2file.js
@@ -10,60 +10,46 @@ USE: node wegner2file.js 8765
 //Requires & Default Settings 
 const fs = require("fs");
 const net = require('net');
-let port = 5001;
-const sngData = 'nowplaying.txt';
+const port = process.argv[2] || 5001;
+const sngData = process.argv[3] || 'nowplaying.txt';
 let data;
-
-if (process.argv[2]) {
-    port = process.argv[2];
-} else {
-    console.log("Using default port")
-}
 
 
 // ****************  Server  ****************
 
 const server = net.createServer((c) => {
-      console.log('Client connected');
+    console.log('Client connected');
     c.on('end', () => {
         console.log('Client disconnected');
     });
-
-    c.on('data', function(chunk) {
+    c.on('data', function (chunk) {
         writeCaptureToFile(chunk.toString());
     });
-
-    c.write('Hello World\r\n');
-
 });
 
 server.on('error', (err) => {
-    console.log('Error: ${err}');
+    console.log(`Error: ${err}`);
     throw err;
 });
 
 server.listen(port, () => {
-    console.log(`Server Online: ${port}`);
+    console.log(getLogTime() + `: Server Online. Port: ${port}, Output File: ${sngData}`);
+
 });
 
 // ****************  Server  ****************
 
 
-//Returns current time for use with logging
-function getLogTime(){   
-    let today = new Date();
-    let hour = (today.getHours() < 10) ? '0' + today.getHours() : today.getHours();
-    let minutes = (today.getMinutes() < 10) ? '0' + today.getMinutes() : today.getMinutes();
-    let seconds = (today.getSeconds() < 10) ? '0' + today.getSeconds() : today.getSeconds();
-    var logtime = hour + ':' + minutes + ':' + seconds;
+function getLogTime() {
+    let logtime = new Date().toISOString();
 
-    return logtime; 
+    return logtime;
 }
 
 // Write output file
-function writeCaptureToFile(data){
+function writeCaptureToFile(data) {
     fs.writeFile(sngData, data, (err) => {
-    if (err) console.log(err);
-    console.log(getLogTime() + ":" + data);
+        if (err) console.log(err);
+        console.log(getLogTime() + ": " + data);
     });
 }
