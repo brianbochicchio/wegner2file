84a5624d01845c4613d11828cce9676b881b7465
Brian Bochicchio <bbochicchio@gmail.com>
2023-08-23 09:06:04 -0500

    Improved bad input checking
    Added additional check for the correct pattern.  Had to use * to allow for punctuation in title.   This can probably be improved.

--- wegner2file.js
+++ wegner2file.js
@@ -10,63 +10,61 @@ USE: node wegner2file.js 8765
 
 */
 
-//Requires & Default Settings 
+//Requires & Default Settings
 const fs = require("fs");
-const net = require('net');
+const net = require("net");
 const port = process.argv[2] || 5001;
-const sngData = process.argv[3] || 'nowplaying.txt';
+const sngData = process.argv[3] || "nowplaying.txt";
 let data;
-
 
 // ****************  Server  ****************
 
 const server = net.createServer((c) => {
-    console.log('Client connected');
-    c.on('end', () => {
-        console.log('Client disconnected');
-    });
-    c.on('data', function (chunk) {
-            createOutputString(chunk.toString());
-    });
+  console.log("Client connected");
+  c.on("end", () => {
+    console.log("Client disconnected");
+  });
+  c.on("data", function (chunk) {
+    createOutputString(chunk.toString());
+  });
 });
 
-server.on('error', (err) => {
-    console.log(`Error: ${err}`);
-    throw err;
+server.on("error", (err) => {
+  console.log(`Error: ${err}`);
+  throw err;
 });
 
 server.listen(port, () => {
-    console.log(getLogTime() + `: Server Online. Port: ${port}, Output File: ${sngData}`);
-
+  console.log(
+    getLogTime() + `: Server Online. Port: ${port}, Output File: ${sngData}`
+  );
 });
 
 // ****************  Server  ****************
 
-
 function getLogTime() {
-    let logtime = new Date().toISOString();
+  let logtime = new Date().toISOString();
 
-    return logtime;
+  return logtime;
 }
 
-function createOutputString(data){
-    data = data.replace(/[\u0000-\u001F\u007F-\u009F]/g, "") // strip control characters - may result in empty string
-    if (data){
-        let input_details = data.split(';');
-        let song_info = `^${input_details[0]}~${input_details[1]}~100~SONG~03:30|`;
-        writeCaptureToFile(song_info)
-    }
-    else {
-        console.log("Received Empty Input")
-    }
-    
-    
+function createOutputString(data) {
+  const regex = new RegExp("\\w*;\\w*");
+  data = data.replace(/[\u0000-\u001F\u007F-\u009F]/g, ""); // strip control characters
+
+  if (regex.test(data)) {
+    let input_details = data.split(";");
+    let song_info = `^${input_details[0].trim()}~${input_details[1].trim()}~100~SONG~03:30|`;
+    writeCaptureToFile(song_info);
+  } else {
+    console.log(`Invalid Input: ${data}`);
+  }
 }
 
 // Write output file
 function writeCaptureToFile(song_info) {
-    fs.writeFile(sngData, song_info, (err) => {
-        if (err) console.log(err);
-        console.log(getLogTime() + ": " + song_info);
-    });
+  fs.writeFile(sngData, song_info, (err) => {
+    if (err) console.log(err);
+    console.log(getLogTime() + ": " + song_info);
+  });
 }
